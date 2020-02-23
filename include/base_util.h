38b0875fa73216688ac8efc29d2fb6e96c2cedf1
drenehtsral <drenehtsral@gmail.com>
2020-02-23 14:43:39 -0800

    Added utility mmap() and munmap() wrappers.

--- include/base_util.h
+++ include/base_util.h
@@ -26,14 +26,24 @@ void _debug_print_vec(const void *data, const unsigned nlanes,
 #define HUGE_1G_SIZE    (1U << HUGE_1G_SHIFT)
 #define HUGE_1G_MASK    (HUGE_1G_SIZE - 1)
 
+#define SEG_DESC_INITD          (1 << 0)    // Segment descriptor is pre-initialized
+#define SEG_DESC_RO             (1 << 1)    // Segment should be mapped read-only
+#define SEG_DESC_UNLINK         (1 << 3)    // Segment should be unlinked after mapping
+#define SEG_DESC_CREATE         (1 << 4)    // Segment should be created
+#define SEG_DESC_ADDR_HINT      (1 << 5)    // Segment should try to map at supplied addr
+#define SEG_DESC_ADDR_FIXED     (1 << 6)    // Segment must be mapped at supplied address
+#define SEG_DESC_ANON           (1 << 7)    // Segment should be allocated via anonymous mmap()
+
 typedef struct {
-    u64 maplen, psize;
+    u64 maplen, psize, flags;
     void *ptr;
 } seg_desc_t;
 
 void consume_data(const void * const RESTR data, const size_t len);
 int randomize_data(void * const RESTR data, const size_t len);
 int get_page_size(const char *path);
-seg_desc_t map_segment(const char *path);
+
+int map_segment(const char *path, seg_desc_t *seg, char *errbuf, const unsigned eblen);
+int unmap_segment(const seg_desc_t * const seg);
 
 #endif /* _BASE_UTIL_H_ */
--- src/base_util.c
+++ src/base_util.c
@@ -6,6 +6,8 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <errno.h>
+#include <libgen.h>
+#include <string.h>
 
 #include "../include/simd_util.h"
 
@@ -128,41 +130,272 @@ int get_page_size(const char *path)
     return (st.st_blksize <= PAGE_SIZE) ? PAGE_SIZE : st.st_blksize;
 }
 
-seg_desc_t map_segment(const char *path)
+int map_segment(const char *path, seg_desc_t *seg, char *errbuf, const unsigned eblen)
 {
-    seg_desc_t tmp = {};
-    const int fd = open(path, O_RDWR);
-
-    if (fd < 0) {
-        return tmp;
-    }
-
+    char dirtmp[1024] = {};
+    char *dir;
+    int fd = -1, ret = 0;
     struct stat st = {};
 
-    fstat(fd, &st);
-
-    tmp.psize = (st.st_blksize <= PAGE_SIZE) ? PAGE_SIZE : st.st_blksize;
-
-    tmp.maplen = st.st_size & ~(tmp.psize - 1);
-
-    tmp.ptr = mmap(NULL, tmp.maplen, PROT_READ | PROT_WRITE,
-                   MAP_SHARED | MAP_POPULATE, fd, 0);
-
-    if (tmp.ptr == MAP_FAILED) {
-        tmp.ptr = NULL;
+    if (eblen && !errbuf) {
+        return -1;
+    } else if (errbuf) {
+        memset(errbuf, 0, eblen);
     }
 
-    close(fd);
-    return tmp;
+    if (seg == NULL) {
+        if (errbuf) {
+            snprintf(errbuf, eblen - 1, "seg must not be NULL.");
+        }
+
+        return -1;
+    }
+
+    if (!(seg->flags & SEG_DESC_INITD)) {
+        memset(seg, 0, sizeof(*seg));
+
+        if (path == NULL) {
+            if (errbuf) {
+                snprintf(errbuf, eblen - 1, "path OR initialized seg must be specified.");
+            }
+
+            return -1;
+        }
+
+        if (access(path, R_OK)) {
+            if (errbuf) {
+                const typeof(errno) err_tmp = errno;
+                snprintf(errbuf, eblen - 1, "Cannot read file %s: %s", path, strerror(err_tmp));
+            }
+
+            return -1;
+        }
+
+        seg->flags = SEG_DESC_INITD;
+
+        if (access(path, W_OK)) {
+            seg->flags |= SEG_DESC_RO;
+        }
+    }
+
+
+    if (path) {
+        if (seg->flags & SEG_DESC_CREATE) {
+            if (stat(path, &st) == 0) {
+                unlink(path);
+            }
+
+            strncpy(dirtmp, path, sizeof(dirtmp) - 1);
+            dir = dirname(dirtmp);
+            ret = stat(dir, &st);
+
+            if (ret < 0) {
+                const typeof(errno) err_tmp = errno;
+                snprintf(errbuf, eblen - 1, "Cannot stat directory %s: %s",
+                         dirtmp, strerror(err_tmp));
+                return -1;
+            }
+
+            const u64 psize = (st.st_blksize <= PAGE_SIZE) ? PAGE_SIZE : st.st_blksize;
+            const u64 pmask = psize - 1;
+            seg->maplen = (seg->maplen + pmask) & ~pmask;
+
+            fd = open(path, O_CREAT | O_RDWR, 0600);
+
+            if (fd < 0) {
+                if (errbuf) {
+                    const typeof(errno) err_tmp = errno;
+                    snprintf(errbuf, eblen - 1, "Cannot create file %s: %s",
+                             path, strerror(err_tmp));
+                }
+
+                return -1;
+            }
+
+            ret = posix_fallocate(fd, 0, seg->maplen);
+
+            if (ret < 0) {
+                if (errbuf) {
+                    const typeof(errno) err_tmp = errno;
+                    snprintf(errbuf, eblen - 1, "Cannot allocate file %s to size 0x%lx: %s",
+                             path, seg->maplen, strerror(err_tmp));
+                }
+
+                return -1;
+            }
+        } else {
+            fd = open(path, (seg->flags & SEG_DESC_RO) ? O_RDONLY : O_RDWR);
+
+            if (fd < 0) {
+                if (errbuf) {
+                    const typeof(errno) err_tmp = errno;
+                    snprintf(errbuf, eblen - 1, "Cannot open file %s for %s: %s",
+                             path, (seg->flags & SEG_DESC_RO) ? "read" : "read+write",
+                             strerror(err_tmp));
+                }
+
+                return -1;
+            }
+        }
+
+        ret = fstat(fd, &st);
+        seg->psize = (st.st_blksize <= PAGE_SIZE) ? PAGE_SIZE : st.st_blksize;
+        seg->maplen = st.st_size & ~(seg->psize - 1);
+
+        if (ret < 0) {
+            if (errbuf) {
+                const typeof(errno) err_tmp = errno;
+                snprintf(errbuf, eblen - 1, "Cannot stat file %s: %s",
+                         path, strerror(err_tmp));
+            }
+
+            return -1;
+        }
+
+        if (seg->flags & SEG_DESC_UNLINK) {
+            unlink(path);
+        }
+    } else {
+        seg->flags |= SEG_DESC_ANON;
+    }
+
+    const int req_prot = PROT_READ | ((seg->flags & SEG_DESC_RO) ? 0 : PROT_WRITE);
+    const int req_flags = MAP_SHARED | ((path == NULL) ? MAP_ANON : 0) | MAP_POPULATE |
+                          ((seg->flags & SEG_DESC_ADDR_FIXED) ? MAP_FIXED : 0);
+
+    void * const req_ptr = (seg->flags & (SEG_DESC_ADDR_FIXED | SEG_DESC_ADDR_HINT)) ?
+                           seg->ptr : NULL;
+
+    seg->ptr = mmap(req_ptr, seg->maplen, req_prot, req_flags, fd, 0);
+
+    printf("mmap(%p, 0x%lx, 0x%x, 0x%x, %d, 0) --> %p\n", req_ptr, seg->maplen, (unsigned)req_prot,
+           (unsigned)req_flags, fd, seg->ptr);
+
+    if (seg->ptr == MAP_FAILED) {
+        if (errbuf) {
+            const typeof(errno) err_tmp = errno;
+            snprintf(errbuf, eblen - 1, "Cannot mmap() file %s from 0 to 0x%lx: %s",
+                     path, seg->maplen, strerror(err_tmp));
+        }
+
+        seg->ptr = NULL;
+    }
+
+    if (fd >= 0) {
+        close(fd);
+    }
+
+    if (seg->ptr == NULL) {
+        if (errbuf && !*errbuf) {
+            snprintf(errbuf, eblen - 1, "Something went sideways: "
+                     "prot=%s maplen=0x%lx psize=0x%lx vaddr=%p flags=0x%lx",
+                     (seg->flags & SEG_DESC_RO) ? "RO" : "RW",
+                     seg->maplen, seg->psize, seg->ptr, seg->flags);
+        }
+
+        return -1;
+    } else {
+        if (errbuf) {
+            snprintf(errbuf, eblen - 1, "OK: prot=%s"
+                     " maplen=0x%lx psize=0x%lx vaddr=%p flags=0x%lx",
+                     (seg->flags & SEG_DESC_RO) ? "RO" : "RW",
+                     seg->maplen, seg->psize, seg->ptr, seg->flags);
+        }
+
+        return 0;
+    }
+}
+
+int unmap_segment(const seg_desc_t * const seg)
+{
+    if (seg == NULL) {
+        return -1;
+    }
+
+    return munmap(seg->ptr, seg->maplen);
 }
 
 #if STANDALONE_TEST
 
+static void usage(const char *pn)
+{
+    printf("Usage:\n\t%s fname [size [addr [flags]]]\n", pn);
+    printf("\tWhere fname is the file to map or \"\" for an anonymous mapping.\n");
+    printf("\tSpecifying non-zero size implies the SEG_DESC_CREATE flag.\n");
+    printf("\tSpecifying a non-NULL address implies SEG_DESC_ADDR_HINT.\n");
+    printf("\tAvailable flags available in arg 4 are:\n");
+    printf("\t\t F --> SEG_DESC_ADDR_FIXED\n");
+    printf("\t\t U --> SEG_DESC_UNLINK\n");
+    printf("\t\t R --> SEG_DESC_RO\n");
+    exit(1);
+}
+
 int main(int argc, char **argv)
 {
-    seg_desc_t seg = map_segment("/dev/hugepages/foobar");
+    char errbuf[1024];
+    seg_desc_t _seg = {};
+    seg_desc_t *seg = &_seg;
+    char *path;
 
-    printf("ptr = %p  maplen = 0x%lx psize = 0x%lx\n", seg.ptr, seg.maplen, seg.psize);
+    if ((argc < 2) | (argc > 5)) {
+        usage(argv[0]);
+    }
+
+    path = argv[1];
+
+    if (path[0] == '\0') {
+        path = NULL;
+    }
+
+    if (argc >= 3) {
+        seg->maplen = strtoul(argv[2], NULL, 0);
+        seg->flags |= (seg->maplen != 0) ? (SEG_DESC_CREATE | SEG_DESC_INITD) : 0;
+    }
+
+    if (argc >= 4) {
+        seg->ptr = (void *) strtoul(argv[3], NULL, 0);
+        seg->flags |= (seg->ptr != NULL) ? (SEG_DESC_ADDR_HINT | SEG_DESC_INITD) : 0;
+    }
+
+    if (argc == 5) {
+        const char *trav = argv[4];
+        seg->flags |= SEG_DESC_INITD;
+
+        while (1) {
+            const char flg = *(trav++);
+
+            if (flg == '\0') {
+                break;
+            }
+
+            switch(flg) {
+                case 'F':
+                    seg->flags |= SEG_DESC_ADDR_FIXED;
+                    break;
+
+                case 'U':
+                    seg->flags |= SEG_DESC_UNLINK;
+                    break;
+
+                case 'R':
+                    seg->flags |= SEG_DESC_RO;
+                    break;
+
+                default:
+                    printf("Unknown flag character '%c'\n", flg);
+                    exit(1);
+            }
+        }
+    }
+
+    int ret = map_segment(path, seg, errbuf, sizeof(errbuf));
+
+    printf("%d -- %s\n", ret, errbuf);
+
+    ret = unmap_segment(seg);
+
+    printf("%d -- %s\n", ret, ret ? "Unmap failed" : "Unmap OK");
+
     return 0;
 }
 
